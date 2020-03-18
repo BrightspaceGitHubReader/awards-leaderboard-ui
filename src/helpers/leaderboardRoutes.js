e620922b1c3ff7429d14aa67025f8c0500e09f18
Christine Belan <Christine.Belan@D2L.com>
2020-03-18 13:35:00 -0400

    Use new issued award routee

--- src/helpers/leaderboardRoutes.js
+++ src/helpers/leaderboardRoutes.js
@@ -3,7 +3,7 @@ const MAX_USERS = 10;
 
 export class LeaderboardRoutes {
 	static ClasslistLeaderboard(orgUnitId, classlistSort) { return `/d2l/api/bas/${BAS_VERSION}/orgunits/${orgUnitId}/classlist/?limit=${MAX_USERS}&sortField=${classlistSort}`; }
-	static GetIssuedAward(userId, issueId) { return `/d2l/awards/profile/${userId}/issue/${issueId}`; }
+	static GetIssuedAward(userId, issueId) { return `/d2l/awards/profile/${userId}/issue/${issueId}/page`; }
 	static MyAwards(orgUnitId, userId) { return `/d2l/api/bas/${BAS_VERSION}/orgunits/${orgUnitId}/classlist/users/${userId}`; }
 	static MyConfigs(orgUnitId) { return `/d2l/api/bas/${BAS_VERSION}/orgunits/${orgUnitId}/myConfiguration`; }
 	static ProfileImage(userId) { return `/d2l/api/hm/users/${userId}`; }
